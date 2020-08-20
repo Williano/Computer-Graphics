cd5faa5ba0bdd195187f8ab7242dbfd4b5d22c91
Williano <paawilly17@gmail.com>
2020-08-20 17:57:27 -0500

    Setup up window for rendering

--- Hello_Triangle/Hello_Triangle.cpp
+++ Hello_Triangle/Hello_Triangle.cpp
@@ -4,13 +4,71 @@
 #include <GLFW/glfw3.h>
 
 // Function Prototypes
-
+void framebufferResizeCallback(GLFWwindow* window, int width, int height);
+void processUserInput(GLFWwindow* window);
 
 // Settings constants
-
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
 
 int main() {
 
+	if (!glfwInit())
+	{
+		std::cout << "GLFW failed to initialize. Terminating now....." << std::endl;
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hellow Triangle", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebufferResizeCallback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		//input
+		processUserInput(window);
+
+		// rendering
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Swap buffers and poll events like key pressed, mouse movements
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+	glfwTerminate();
 
 	return 0;
+}
+
+void framebufferResizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processUserInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
 }
