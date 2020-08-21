f51649fd72edf988396e5260a91f7079a4114c71
Williano <paawilly17@gmail.com>
2020-08-21 11:27:36 -0500

    Add vertex shader

--- Hello_Triangle/Hello_Triangle.cpp
+++ Hello_Triangle/Hello_Triangle.cpp
@@ -61,6 +61,22 @@ int main() {
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	// Create Vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
 
 	while (!glfwWindowShouldClose(window))
 	{
