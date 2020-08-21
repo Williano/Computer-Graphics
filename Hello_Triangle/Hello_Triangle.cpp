c4ea6b7b7a06806e69cf273f5948027e608d9750
Williano <paawilly17@gmail.com>
2020-08-21 12:53:07 -0500

    Refactor

--- Hello_Triangle/Hello_Triangle.cpp
+++ Hello_Triangle/Hello_Triangle.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Function Prototypes
+void framebufferResizeCallback(GLFWwindow* window, int width, int height);
+void processUserInput(GLFWwindow* window);
+
+// Settings constants
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
 const char* vertexShaderSource = "#version 460 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
@@ -14,18 +27,6 @@ const char* fragmentShaderSource = "#version 460 core\n"
 "}\0";
 
 
-#include <iostream>
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-// Function Prototypes
-void framebufferResizeCallback(GLFWwindow* window, int width, int height);
-void processUserInput(GLFWwindow* window);
-
-// Settings constants
-const unsigned int SCREEN_WIDTH = 800;
-const unsigned int SCREEN_HEIGHT = 600;
 
 int main() {
 
@@ -55,20 +56,6 @@ int main() {
 		return -1;
 	}
 
-	// Vertex shader input
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f, 0.5f, 0.0f
-	};
-
-	// Vertex Buffer
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	// Create Vertex shader
 	unsigned int vertexShader;
@@ -116,12 +103,34 @@ int main() {
 		std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
 
+	// Vertex shader input
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f, // left
+		 0.5f, -0.5f, 0.0f, // right
+		 0.0f, 0.5f, 0.0f  // top
+	};
+
+	// Vertex Buffer
+	unsigned int VBO, VAO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), 
+	// and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 	// Specify the attributes of the vertex
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// Use the shader for rendering
-	glUseProgram(shaderProgram);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -132,15 +141,19 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// Use the shader for rendering the triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		// Swap buffers and poll events like key pressed, mouse movements
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-
-	// Delete shaders
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 	glfwTerminate();
 
