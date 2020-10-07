90e04fc1d2787d2327107f9afb10441268dea518
Williano <paawilly17@gmail.com>
2020-10-07 15:29:53 -0500

    Add vertex shader

--- OpenGL/Src/Application.cpp
+++ OpenGL/Src/Application.cpp
@@ -8,6 +8,13 @@
 const unsigned int SCREEN_WIDTH = 800;
 const unsigned int SCREEN_HEIGHT = 600;
 
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\n";
 
 /*** Constants **/
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
