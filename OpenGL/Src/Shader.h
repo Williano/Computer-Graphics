7641fca222ed8a6490e19b2f04e2915df6d0c694
Williano <paawilly17@gmail.com>
2020-10-12 17:57:39 -0500

    Add new method

--- OpenGL/Src/Shader.h
+++ OpenGL/Src/Shader.h
@@ -11,22 +11,28 @@
 class Shader {
 
 public:
-	// shader program ID
-	unsigned int m_shaderID;
 
 	// reads and builds the shader
 	Shader(const char* vertexPath, const char* fragmentPath);
 
 	void CreateShader(unsigned int shaderType, const std::string& shaderSource);
-
-	void CreateShaderProgram();
-
-	// use/activate the shader
-	void use();
+	void deleteShader();
+	void CreateShaderProgram(unsigned int shaderID);
+	void useShaderProgram();
+	void deleteShaderProgram();
 
 	// utiltiy uniform functions
 	void setBool(const std::string& name, bool value) const;
-	void setInt(const std::string* name, bool value) const;
-	void setFloat(const std::string*& name, float value) const;
+	void setInt(const std::string& name, int value) const;
+	void setFloat(const std::string& name, float value) const;
+
+private:
+
+	unsigned int m_ShaderProgramID;
+	const char* m_vertexShaderCode;
+	const char* m_fragmentShaderCode;
+	unsigned int m_shaderID;
+	int m_success;
+	char m_infoLog[512];
 
 };
